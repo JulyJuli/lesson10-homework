a72bb0e83f23e98e7b8b42f7e2270aa4e93cdd4a
Roman <roma.ne4aev2013@gmail.com>
2020-03-26 22:53:52 +0200

    HW_10

---
+++ Roman Nechaiev/Roman Nechaiev/BasicProduct.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roman_Nechaiev
+{
+    public class BasicProduct:Product
+    {
+        public BasicProduct(DateTime deliveryDate, int shelfLife, string nameOfProguct, decimal price) :base(nameOfProguct, price)
+        {
+            DeliveryDate = deliveryDate;
+            ShelfLife = shelfLife;
+        }
+
+        public DateTime DeliveryDate { get; set; }
+        public int ShelfLife { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString()+$"Date of Delivery: {DeliveryDate.ToShortDateString()}, ShelfLife: {ShelfLife} days\n";
+        }
+    }
+}
---
+++ Roman Nechaiev/Roman Nechaiev/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roman_Nechaiev
+{
+    public class Product
+    {
+        public Product(string nameOfProguct, decimal price)
+        {
+            NameOfProguct = nameOfProguct;
+            Price = price;            
+        }
+
+        public string NameOfProguct { get; set; }
+        public decimal Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name:{NameOfProguct}, Price: {Price},";
+        }
+    }
+}
