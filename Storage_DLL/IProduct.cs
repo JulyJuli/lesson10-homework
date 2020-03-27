42264795c0ff6de218dea0a03d45ab7e0e2fcec6
smokeymoisha <rftg347@gmail.com>
2020-03-27 19:17:43 +0200

    lesson 10 homework

---
+++ Storage_DLL/IProduct.cs
@@ -0,0 +1,9 @@
+﻿namespace Storage_DLL
+{
+    public interface IProduct
+    {
+        string Name { get; set; }
+        int Price { get; set; }
+        BaseProduct BaseProduct { get; set; }
+    }
+}
---
+++ Storage_DLL/Storage.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Storage_DLL
+{
+    public class Storage
+    {
+        private int _maxNumOfProducts = 0;
+        private List<Product> _productList;
+        public int MaxNumOfProducts
+        {
+            get => _maxNumOfProducts;
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Invalid number of products!");
+                }
+                else
+                {
+                    _maxNumOfProducts = value;
+                }
+            }
+        }
+        public List<Product> ProductList
+        {
+            get => _productList;
+            set
+            {
+                if (value.Count > MaxNumOfProducts)
+                {
+                    Console.WriteLine("Storage limit exceeded!");
+                }
+                else
+                {
+                    _productList = value;
+                }
+            }
+        }
+
+        public Storage(int maxNumOfProducts)
+        {
+            this.MaxNumOfProducts = maxNumOfProducts;
+        }
+
+        public override string ToString()
+        {
+            var storageString = new StringBuilder();
+
+            foreach (Product product in ProductList)
+            {
+                storageString.AppendLine(product.ToString());
+            }
+
+            return storageString.ToString();
+        }
+
+    }
+}
