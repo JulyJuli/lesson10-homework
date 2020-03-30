4385057d7d271eca791a04df99b7c9ce6ea57db1
e.repin <mr.evgeniy.repin@gmail.com>
2020-03-30 11:48:57 +0300

    comment

---
+++ HomeWork10/HW2403/MainMethods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HW2403
+{
+    class MainMethods
+    {
+        public static double ValidationStr(string str)
+        {
+            double number;
+            while (!double.TryParse(str, out number))
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("Insert the number. Try again: ");
+                Console.ResetColor();
+                str = Console.ReadLine();
+            }
+            return number;
+        }         
+
+    }
+}
